a5b95dc291ee302eb0ced911ffacde907538d0a5
Inayat Yousuf <inayatyousuf77@gmail.com>
2022-10-05 15:03:27 +0530

    added popup for new-sale page, franchises page and for investor page

--- Huntment-new/business_for_sale/new_sale.js
+++ Huntment-new/business_for_sale/new_sale.js
@@ -59,7 +59,35 @@ const setLabel = (lbl, val) => {
   }
   
   
-  $('#ex2').slider().on('slide', function(ev) {
-    setLabels(ev.value);
-  });
-  setLabels($('#ex2').attr("data-value").split(","));
+  // $('#ex2').slider().on('slide', function(ev) {
+  //   setLabels(ev.value);
+  // });
+  // setLabels($('#ex2').attr("data-value").split(","));
+
+
+  // popup js 
+
+  const loginPopup = document.querySelector(".login-popup");
+
+
+  window.addEventListener("load",function(){
+ 
+   showPopup();
+  //  setTimeout(function(){
+  //    loginPopup.classList.add("show");
+  //  },5000)
+
+  })
+
+  function showPopup(){
+        const timeLimit = 5 // seconds;
+        let i=0;
+        const timer = setInterval(function(){
+         i++;
+         if(i == timeLimit){
+          clearInterval(timer);
+          loginPopup.classList.add("show");
+           document.body.style.overflow='hidden';
+         } 
+        },1000);
+  }
--- Huntment-new/investors&buyers/investor.js
+++ Huntment-new/investors&buyers/investor.js
@@ -2,3 +2,30 @@ function show_rating(rating)
 {
  document.getElementById("rating_value").innerHTML= rating;
 }
+
+  // popup js 
+
+  const loginPopup = document.querySelector(".login-popup");
+
+
+  window.addEventListener("load",function(){
+ 
+   showPopup();
+  //  setTimeout(function(){
+  //    loginPopup.classList.add("show");
+  //  },5000)
+
+  })
+
+  function showPopup(){
+        const timeLimit = 5 // seconds;
+        let i=0;
+        const timer = setInterval(function(){
+         i++;
+         if(i == timeLimit){
+          clearInterval(timer);
+          loginPopup.classList.add("show");
+           document.body.style.overflow='hidden';
+         } 
+        },1000);
+  }
